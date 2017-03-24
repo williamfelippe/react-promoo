f948d88ef1910d00626ebfa57501b37aa68d8210
William Felippe F. da Silva <wfelippesilva@gmail.com>
2017-03-23 23:59:53 -0300

    Decide the way create store will be

--- src/components/create-store/create-store-form/create-store-form.js
+++ src/components/create-store/create-store-form/create-store-form.js
@@ -1,24 +1,68 @@
 import React, {Component} from "react";
 import {Row, Col, Input, Icon, Button} from "react-materialize";
 import PlacesAutocomplete, {geocodeByPlaceId} from "react-places-autocomplete";
-import StreetSuggest from "../street-suggest/street-suggest";
-import "./create-store-form.css";
+import {getStoreCategories} from "../../../services/store-service";
+import {publishMessage} from "../../../utils/messages-publisher";
+import Loader from "../../util/loader/loader";
+import "../create-store-form/create-store-form.css";
 
-export default class CreateStoreForm extends Component {
+export default class CreateStoreFormTest extends Component {
     constructor(props) {
         super(props);
         this.state = {
             name: '',
+            category: '',
             city: '',
             cityId: '',
-            address: '',
-            cityLat: 0,
-            cityLng: 0
+            street: '',
+            state: '',
+            neighborhood: '',
+            lat: 0,
+            lng: 0,
+            storeCategories: [],
+            loadingCategories: false
         };
+    }
+
+    componentDidMount() {
+        this.getCategories();
+    }
+
+    getCategories() {
+        this.setState({loadingCategories: true});
+
+        getStoreCategories()
+            .then((response) => {
+                console.log(response);
+                const status = response.status;
+
+                if(status === 200) {
+                    this.setState({storeCategories: response.data})
+                }
+                else {
+                    throw new Error(response.data);
+                }
+
+                this.setState({loadingCategories: false});
+            })
+            .catch((error) => {
+                console.log(error);
+                publishMessage("Ops... Parece que estamos com alguns problemas");
+
+                this.setState({loadingCategories: false});
+            });
     }
 
     onChangeName(event) {
         this.setState({name: event.target.value});
+    }
+
+    onChangeStoreCategory(event) {
+        this.setState({category: event.target.value});
+    }
+
+    onChangeNeighborhood(event) {
+        this.setState({neighborhood: event.target.value});
     }
 
     onChangeCity(city) {
@@ -27,34 +71,21 @@ export default class CreateStoreForm extends Component {
 
     onSelectCity(city, cityId) {
         console.log(`Cidade selecionada: ${city} - ${cityId}`);
-        this.setState({city, cityId});
 
         geocodeByPlaceId(cityId, (error, {lat, lng}, results) => {
             if (error) {
                 return
             }
 
-            this.setState({cityLat: lat, cityLng: lng});
+            this.setState({
+                city, cityId,
+                lat: lat,
+                lng: lng,
+                street: results[0].address_components[0].long_name,
+                state: results[0].address_components[2].short_name
+            })
 
-            console.log('Geocoding success!', {lat, lng});
-            console.log('Entire payload from Google API', results);
-        });
-    }
-
-    onChangeAddress(address) {
-        this.setState({address});
-    }
-
-    onSelectAddress(address, addressId) {
-        console.log(`Endereço selecionado: ${address} - ${addressId}`);
-        this.setState({address, addressId});
-
-        geocodeByPlaceId(addressId, (error, {lat, lng}, results) => {
-            if (error) {
-                return
-            }
-
-            console.log(results);
+            console.log('Novo estado ' + this.state);
         });
     }
 
@@ -69,6 +100,12 @@ export default class CreateStoreForm extends Component {
             componentRestrictions: {'country': 'br'}
         };
 
+        const listCategories = this.state.storeCategories.map((storeCategory) =>
+            <option value={storeCategory._id} key={storeCategory._id}>
+                {storeCategory.name}
+            </option>
+        );
+
         return (
             <Row className="moo-create-store">
                 <form onSubmit={this.submit.bind(this)} className="col s12">
@@ -81,6 +118,19 @@ export default class CreateStoreForm extends Component {
                             <Input s={12} type="text" label="Nome da loja" onChange={this.onChangeName.bind(this)}/>
                         </Col>
                     </Row>
+
+                    <Row>
+                        {
+                            /* Categoria do produto */
+                            (this.state.loadingCategories) ?
+                            <Loader /> :
+                            <Input s={12} type="select" defaultValue="" label="Escolha uma categoria" 
+                                onChange={this.onChangeStoreCategory.bind(this)}>
+                                <option value="" disabled>Escolhe uma aí =)</option>
+                                {listCategories}
+                            </Input>
+                        }
+                     </Row>
 
                     <Row>
                         <Col s={12} className="n-padding">
@@ -98,24 +148,23 @@ export default class CreateStoreForm extends Component {
                                     onChange={this.onChangeCity.bind(this)}
                                     onSelect={this.onSelectCity.bind(this)} options={options}
                                     placeholder="&nbsp;" hideLabel>
+                                    
                                     <Input s={12} label="Procurar por endereço" className="m-l-20 m-r-20"/>
                                 </PlacesAutocomplete>
                             </div>
                         </Col>
                     </Row>
 
-                    <Row>
-                        <Col s={12}>
-                            {
-                                (this.state.cityLng !== 0 && this.state.cityLat !== 0) &&
-                                <StreetSuggest
-                                    address={this.state.address}
-                                    onChangeAddress={this.onChangeAddress.bind(this)}
-                                    onSelectAddress={this.onSelectAddress.bind(this)}
-                                    lat={this.state.cityLat} lng={this.state.cityLng}/>
-                            }
-                        </Col>
-                    </Row>
+                    {
+                        (this.city) &&
+                        <Row>
+                            <Input s={12} type="text" label="Bairro" onChange={this.onChangeNeighborhood.bind(this)}/>
+
+                            <p className="help">
+                                Em qual bairro fica essa loja/supermercado
+                            </p>
+                        </Row>
+                    }
 
                     <Row>
                         <Col s={12}>
