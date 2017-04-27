b2f249ec53a6a7ddb785ed1bd4fe19af1ac0bff2
William Felippe F. da Silva <wfelippesilva@gmail.com>
2017-04-27 04:09:08 -0300

    Insert about text

--- src/containers/about/about.js
+++ src/containers/about/about.js
@@ -7,30 +7,31 @@ export default class About extends Component {
         return (
             <div className="container">
                 <Row>
-                    <Col s={10} m={8} offset="s1 m2" className="center-align">
+                    <Col s={10} m={8} offset="s1 m2" className="moo-about-text center-align">
                         <h2>Sobre</h2>
 
-                        <p className="moo-about-text">
-                            Sed rhoncus dui metus, sit amet lobortis mauris lobortis ut. Etiam pretium dolor
-                            eget elit facilisis ullamcorper. In varius imperdiet velit, sit amet bibendum
-                            libero malesuada a. Nulla consectetur fermentum lacus, vitae venenatis purus
-                            iaculis eget. Suspendisse auctor tincidunt neque, et varius enim tempor id. Ut
-                            quis mollis neque. Curabitur vel massa ut elit viverra euismod. Aenean dapibus
-                            dui a vulputate vehicula. Fusce viverra mattis nisl, vitae malesuada nisl dictum
-                            sed. Nulla ut laoreet lacus.
+                        <p>
+                            O Promoo surgiu em um momento bem família, quando sogro e genro conversavam
+                            sobre promoções de supermercado. Sabendo que economizar é algo que todo mundo
+                            gosta, mas ninguém tem tempo para pesquisar preços em vários lugares, um grupo
+                            de amigos programadores se inspirou nessa conversa e decidiu simplificar essa burocracia da
+                            vida adulta.
                         </p>
-                        <p className="moo-about-text">
-                            Nam a congue nulla, consectetur dignissim odio. Nunc ante quam, volutpat
-                            tincidunt tortor a, venenatis molestie nibh. Ut molestie velit ac nulla rutrum
-                            interdum. Nullam non molestie dolor. Donec hendrerit a diam nec molestie. Proin
-                            ut bibendum odio, vitae tincidunt ex. Nunc dictum nec nisi vel varius.
-                            Vestibulum sit amet nisi at purus luctus tincidunt. Pellentesque auctor erat vel
-                            imperdiet semper. Morbi sit amet fringilla tortor. Integer tempor leo sit amet
-                            nisl viverra faucibus. In hac habitasse platea dictumst. Pellentesque ornare
-                            porta sem vel accumsan. Aenean imperdiet tincidunt elit, a pulvinar dolor semper
-                            quis. Etiam tristique nibh nec odio ullamcorper, pellentesque sodales justo
-                            scelerisque.
+
+                        <h5>
+                            Mas como funciona?
+                        </h5>
+
+                        <p>
+                            É muito fácil! Basta escolher a cidade, o tipo de produto ou o supermercado desejado para
+                            filtrar as promoções e descobrir onde estão os melhores preços. Assim você
+                            garante as melhores ofertas e pode ter mais tempo livre para o que realmente
+                            importa.
                         </p>
+
+                        <h4>
+                            Não perca tempo e experimente essa novidade
+                        </h4>
                     </Col>
                 </Row>
             </div>
